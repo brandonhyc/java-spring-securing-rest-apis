7cf210f13d374aa22f81e4f0f2c029c68176a699
yche <brandon.heyc@gmail.com>
2020-09-30 11:36:48 -0400

    Add CORS

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -28,11 +30,27 @@ public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 		return new UserRepositoryUserDetailsService(users);
 	}
 
+	@Bean
+	WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						// .maxAge(0) // if using local verification
+						.allowedOrigins("http://localhost:4000")
+						.allowedMethods("HEAD")
+						.allowedHeaders("Authorization");
+			}
+		};
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests(authz -> authz
 					.anyRequest().authenticated())
-				.httpBasic(basic -> {});
+				.httpBasic(basic -> {})
+				.cors(cors -> {})
+				;
 	}
 }
