95be34d2dce62e4a6c18156088aeb5921fd06c71
yche <brandon.heyc@gmail.com>
2020-09-29 22:45:07 -0400

    Add user authority

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
@@ -1,9 +1,11 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
 
 
